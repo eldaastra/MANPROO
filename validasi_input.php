8b3138b184d773868a51619167752d00dd211645
tasqiyaa <tasqiyaa28@gmail.com>
2025-11-06 16:05:35 +0700

    Create validasi_input.php

---
+++ validasi_input.php
@@ -0,0 +1,9 @@
+<?php
+function validasiInput($nilai, $jenis, $dari, $ke) {
+    $kelompok = [
+        "panjang" => ["km","hm","dam","m","dm","cm","mm"],
+        "berat"   => ["kg","hg","dag","g","dg","cg","mg"],
+        "suhu"    => ["celsius","fahrenheit","kelvin"]
+    ];
+}
+?>
